3a1d30985f2a6a573231f92410fd9b502e732972
freelancerfume <freelancerfume@gmail.com>
2023-02-14 23:47:59 +0300

    20230214 LWC Examples 2

--- force-app/main/default/lwc/accountComponent/accountComponent.js
+++ force-app/main/default/lwc/accountComponent/accountComponent.js
@@ -2,25 +2,29 @@ import { LightningElement,wire,track } from 'lwc';
 // import apex class method. 
 import retrive from '@salesforce/apex/accountLWCController.retrive';
 export default class AccountComponent extends LightningElement {
+  @track multiaccounts;
+  searchText;
   // wire 
-//@wire(retrive) accounts;
-
-@track multiaccounts;
-
-  // imperative
-
-connectedCallback(){
-
-    retrive()
-    .then(data=>{
-this.multiaccounts=data;
-
-
-    })
-    .catch();
-
-
+ @wire(retrive,{searchterm:'$searchText'}) accounts;
+renderedCallback(){
+  console.log(this.accounts);
 }
-
-
+textChangeHandler(event){
+this.searchText=event.target.value;
+console.log(this.searchText);
+}
+ connectedCallback(){
+  // imperative
+  /*
+  retrive()
+  .then(data=>{
+    console.log('retrieved Accounts');
+    console.log(data);
+this.multiaccounts=data;
+  })
+  .catch(err=>{
+    console.log(err);
+  });
+  */
+ }
 }
