12bad346c53f7d6862bf367638fcc3cd963b8316
freelancerfume <freelancerfume@gmail.com>
2023-02-15 22:34:49 +0300

    20230215 LWC Examples

--- force-app/main/default/lwc/accountComponent/accountComponent.js
+++ force-app/main/default/lwc/accountComponent/accountComponent.js
@@ -1,12 +1,21 @@
 import { LightningElement,wire,track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 // import apex class method. 
 import retrive from '@salesforce/apex/accountLWCController.retrive';
+const columns = [
+  { label: 'Company Name', fieldName: 'Name' },
+  { label: 'Website', fieldName: 'Website', type: 'url' },
+  { label: 'Phone', fieldName: 'Phone', type: 'phone' },
+  { label: 'Industry', fieldName: 'Industry' },
+ 
+];
 export default class AccountComponent extends LightningElement {
   // properties
   @track multiaccounts;
   searchText;
+  columns=columns;
   // wire 
- // @wire(retrive,{searchterm:'$searchText'}) accounts;
+ // @wire(retrive,{searchterm:'$searchText'}) accounts;  // accounts.data
 renderedCallback(){
   console.log(this.accounts);
 }
@@ -14,7 +23,6 @@ textChangeHandler(event){
   this.searchText=event.target.value;
   console.log(this.searchText);
   // imperative
-    
   retrive({
     searchterm:this.searchText
   })
@@ -25,6 +33,13 @@ textChangeHandler(event){
   })
   .catch(err=>{
     console.log(err);
+    const errorevt = new ShowToastEvent({
+          title: 'Error ',
+          message: err ,
+          variant: 'error',
+          mode: 'dismissable'
+      });
+      this.dispatchEvent(errorevt);
   });
 }
  connectedCallback(){
