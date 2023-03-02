b348e4c491b4b7e5048b7cba1380206ce550cc37
freelancerfume <freelancerfume@gmail.com>
2023-03-02 11:25:50 +0300

    20230302 Simple LWC Calculator

---
+++ force-app/main/default/lwc/lwccalculator/lwccalculator.js
@@ -0,0 +1,33 @@
+import { LightningElement } from 'lwc';
+
+export default class Lwccalculator extends LightningElement {
+
+    firstNum;
+    secondNum;
+    result;
+
+    handleNumberOneChange(event){
+        this.firstNum = parseInt(event.target.value);
+    }
+
+    handleNumberTwoChange(event){
+        this.secondNum = parseInt(event.target.value);
+    }
+
+    handleClickSum(){
+        this.result = parseInt(this.firstNum) + parseInt(this.secondNum);
+    }
+
+    handleClickSub(){
+        this.result = parseInt(this.firstNum) - parseInt(this.secondNum);
+    }
+
+    handleClickMul(){
+        this.result = parseInt(this.firstNum) * parseInt(this.secondNum);
+    }
+
+    handleClickDiv(){
+        this.result = parseInt(this.firstNum) / parseInt(this.secondNum);
+    }
+
+}
