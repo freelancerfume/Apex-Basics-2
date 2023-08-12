8a481936c0f7fe500e721df3a791fb7083932691
freelancerfume <freelancerfume@gmail.com>
2023-08-12 05:17:13 +0300

    Calculate

--- force-app/main/default/lwc/lwccalculator/lwccalculator.js
+++ force-app/main/default/lwc/lwccalculator/lwccalculator.js
@@ -9,25 +9,19 @@ export default class Lwccalculator extends LightningElement {
     handleNumberOneChange(event){
         this.firstNum = parseInt(event.target.value);
     }
-
     handleNumberTwoChange(event){
         this.secondNum = parseInt(event.target.value);
     }
-
     handleClickSum(){
         this.result = parseInt(this.firstNum) + parseInt(this.secondNum);
     }
-
     handleClickSub(){
         this.result = parseInt(this.firstNum) - parseInt(this.secondNum);
     }
-
     handleClickMul(){
         this.result = parseInt(this.firstNum) * parseInt(this.secondNum);
     }
-
     handleClickDiv(){
         this.result = parseInt(this.firstNum) / parseInt(this.secondNum);
     }
-
 }
